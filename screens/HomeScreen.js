b454ded1d6826c97c2dd6b949aaca03768108d11
andrea-cyt <158221405+andrea-cyt@users.noreply.github.com>
2024-05-06 23:35:26 -0400

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,17 +7,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?new+york',
     places: [
       {
-        name: '\n1. Central Park\n',
+        name: '\nCentral Park\n',
         description: 'A sprawling green oasis in the heart of Manhattan, offering a peaceful escape from the bustling city life.',
         image: 'https://source.unsplash.com/random/300x300?central+park'
       },
       {
-        name: '\n2. Times Square\n',
+        name: '\nTimes Square\n',
         description: 'The iconic commercial and entertainment hub of NYC, known for its vibrant lights, Broadway theaters, and bustling crowds.',
         image: 'https://source.unsplash.com/random/300x300?times+square'
       },
       {
-        name: '\n3. Statue of Liberty\n',
+        name: '\nStatue of Liberty\n',
         description: 'A symbol of freedom and democracy, located on Liberty Island in New York Harbor. Visitors can take ferry rides to the island and explore the statue up close.',
         image: 'https://source.unsplash.com/random/300x300?statue+liberty'
       }
@@ -29,17 +29,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?san+francisco',
     places: [
       {
-        name: '\n1. Golden Gate Bridge\n',
+        name: '\nGolden Gate Bridge\n',
         description: 'An iconic suspension bridge spanning the Golden Gate strait, connecting San Francisco to Marin County. It is one of the most photographed landmarks in the world.',
         image: 'https://source.unsplash.com/random/300x300?golden+gate+bridge'
       },
       {
-        name: '\n2. Yosemite National Park\n',
+        name: '\nYosemite National Park\n',
         description: 'A UNESCO World Heritage Site known for its stunning granite cliffs, waterfalls, and diverse ecosystems. Visitors can enjoy hiking, rock climbing, and wildlife watching in the park.',
         image: 'https://source.unsplash.com/random/300x300?yosemite+national+park'
       },
       {
-        name: '\n3. Disneyland\n',
+        name: '\nDisneyland\n',
         description: 'A world-famous theme park resort in Anaheim, California, featuring attractions, entertainment, and dining experiences inspired by Disney characters and stories.',
         image: 'https://source.unsplash.com/random/300x300?disney+land'
       }
@@ -51,17 +51,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?florida',
     places: [
       {
-        name: '\n1. Walt Disney World\n',
+        name: '\nWalt Disney World\n',
         description: 'The world\'s most visited vacation resort, featuring four theme parks, two water parks, and various entertainment and dining options for visitors of all ages.',
         image: 'https://source.unsplash.com/random/300x300?walt+disney+world'
       },
       {
-        name: '\n2. Universal Studios\n',
+        name: '\nUniversal Studios\n',
         description: 'A renowned film and television production studio that also operates as a theme park, offering immersive attractions and experiences based on popular movies and TV shows.',
         image: 'https://source.unsplash.com/random/300x300?universal+studio'
       },
       {
-        name: '\n3. Miami Beach\n',
+        name: '\nMiami Beach\n',
         description: 'A vibrant coastal city known for its beautiful beaches, Art Deco architecture, and lively nightlife. Visitors can enjoy sunbathing, water sports, and exploring the vibrant neighborhoods of South Beach and Wynwood.',
         image: 'https://source.unsplash.com/random/300x300?miami+beach'
       }
@@ -73,17 +73,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?texas',
     places: [
       {
-        name: '\n1. The Alamo\n',
+        name: '\nThe Alamo\n',
         description: 'A historic Spanish mission and fortress compound in San Antonio, Texas, known for its pivotal role in the Texas Revolution. Visitors can explore the site and learn about its rich history through exhibits and guided tours.',
         image: 'https://source.unsplash.com/random/300x300?the+alamo'
       },
       {
-        name: '\n2. Space Center Houston\n',
+        name: '\nSpace Center Houston\n',
         description: 'The official visitor center of NASA\'s Johnson Space Center, where visitors can learn about space exploration, view artifacts from space missions, and even meet astronauts.',
         image: 'https://source.unsplash.com/random/300x300?space+center+houston'
       },
       {
-        name: '\n3. San Antonio River Walk\n',
+        name: '\nSan Antonio River Walk\n',
         description: 'A picturesque urban waterway in San Antonio, lined with shops, restaurants, and attractions. Visitors can take boat tours, stroll along the riverwalk, and enjoy the vibrant atmosphere of this iconic destination.',
         image: 'https://source.unsplash.com/random/300x300?san+antonio+river+walk'
       }
@@ -95,17 +95,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?colorado',
     places: [
       {
-        name: '\n1. Rocky Mountain National Park\n',
+        name: '\nRocky Mountain National Park\n',
         description: 'A breathtaking national park renowned for its towering peaks, alpine lakes, and diverse ecosystems. Visitors can enjoy hiking, wildlife viewing, and scenic drives amidst stunning natural beauty.',
         image: 'https://source.unsplash.com/random/300x300?rocky+mountain+national+park'
       },
       {
-        name: '\n2. Garden of the Gods\n',
+        name: '\nGarden of the Gods\n',
         description: 'A unique geological formation featuring towering sandstone rock formations against a backdrop of snow-capped mountains. Visitors can explore hiking trails, rock climb, and marvel at the striking landscape.',
         image: 'https://source.unsplash.com/random/300x300?garden+gods'
       },
       {
-        name: '\n3. Mesa Verde National Park\n',
+        name: '\nMesa Verde National Park\n',
         description: 'Home to ancient Pueblo cliff dwellings and archaeological sites, Mesa Verde National Park offers a fascinating glimpse into the lives of indigenous peoples who inhabited the region over a thousand years ago.',
         image: 'https://source.unsplash.com/random/300x300?mesa+verde+national+park'
       }
@@ -117,17 +117,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?washington',
     places: [
       {
-        name: '\n1. Mount Rainier National Park\n',
+        name: '\nMount Rainier National Park\n',
         description: 'An iconic national park dominated by the towering Mount Rainier, an active stratovolcano. Visitors can explore alpine meadows, hike scenic trails, and marvel at breathtaking views of glaciers and wildflowers.',
         image: 'https://source.unsplash.com/random/300x300?mount+rainer+national+park'
       },
       {
-        name: '\n2. Pike Place Market\n',
+        name: '\nPike Place Market\n',
         description: 'One of the oldest continuously operated public farmers\' markets in the United States, Pike Place Market is a bustling hub of activity offering fresh produce, artisanal goods, and seafood, as well as restaurants and unique shops.',
         image: 'https://source.unsplash.com/random/300x300?pike+place+market]'
       },
       {
-        name: '\n3. Space Needle\n\n',
+        name: '\nSpace Needle\n\n',
         description: 'An iconic landmark of Seattle, the Space Needle offers panoramic views of the city skyline, Puget Sound, and surrounding mountains from its observation deck. Visitors can also dine at the rotating SkyCity restaurant for a unique dining experience.',
         image: 'https://source.unsplash.com/random/300x300?space+needle'
       }
@@ -139,17 +139,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?arizona',
     places: [
       {
-        name: '\n1. Grand Canyon National Park\n',
+        name: '\nGrand Canyon National Park\n',
         description: 'One of the most iconic natural wonders of the world, the Grand Canyon is a breathtaking geological formation carved by the Colorado River over millions of years. Visitors can explore the canyon rim, hike trails, and marvel at the stunning vistas.',
         image: 'https://source.unsplash.com/random/300x300?grand+canyon+national+park'
       },
       {
-        name: '\n2. Sedona\n',
+        name: '\nSedona\n',
         description: 'Famous for its red rock formations and spiritual energy, Sedona offers stunning landscapes, vibrant art galleries, and a range of outdoor activities such as hiking, mountain biking, and jeep tours amidst the stunning red rock scenery.',
         image: 'https://source.unsplash.com/random/300x300?sedona+arizona'
       },
       {
-        name: '\n3. Horseshoe Bend\n',
+        name: '\nHorseshoe Bend\n',
         description: 'A dramatic horseshoe-shaped bend in the Colorado River near the town of Page, Horseshoe Bend offers awe-inspiring views of the meandering river and steep canyon walls. Visitors can hike to the overlook for panoramic vistas and stunning photo opportunities.',
         image: 'https://source.unsplash.com/random/300x300?horsehoe+bend+arizona'
       }
@@ -161,17 +161,17 @@ const locations = [
     image: 'https://source.unsplash.com/random/300x300?nevada',
     places: [
       {
-        name: '\n1. Las Vegas Strip\n',
+        name: '\nLas Vegas Strip\n',
         description: 'The iconic Las Vegas Strip is known for its dazzling array of resorts, casinos, and entertainment venues. Visitors can explore themed hotels, enjoy world-class dining, and experience thrilling nightlife and entertainment options.',
         image: 'https://source.unsplash.com/random/300x300?las+vegas+strip'
       },
       {
-        name: '\n2. Hoover Dam\n',
+        name: '\nHoover Dam\n',
         description: 'An engineering marvel and a symbol of human ingenuity, the Hoover Dam stands as a testament to America\'s ability to tame mighty rivers. Visitors can take guided tours to learn about the dam\'s history, construction, and significance.',
         image: 'https://source.unsplash.com/random/300x300?hoover+dam'
       },
       {
-        name: '\n3. Red Rock Canyon National Conservation Area\n',
+        name: '\nRed Rock Canyon National Conservation Area\n',
         description: 'Located just outside of Las Vegas, Red Rock Canyon offers stunning desert landscapes, towering red sandstone formations, and scenic hiking trails. Visitors can enjoy outdoor activities such as hiking, rock climbing, and wildlife viewing amidst the rugged beauty of the Mojave Desert.',
         image: 'https://source.unsplash.com/random/300x300?red+rock+canyon+national+conservation+area'
       }
@@ -195,7 +195,7 @@ const HomeScreen = ({ navigation }) => {
             <Image source={{ uri: location.image }} style={styles.image} />
             <Text style={styles.locationTitle}>{location.title}</Text>
             <Text style={styles.locationDescription}>{location.places.map(place => place.name).join(' ')}</Text>
-            <Button title="View Details" onPress={() => navigateToInfo(location)} />
+            <Button title="View Details" onPress={() => navigateToInfo(location)} color="darkorange" />
             <View style={styles.separator} />
           </View>
         ))}
@@ -213,7 +213,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightyellow',
   },
   title: {
-    marginTop: 60,
+    marginTop: 30,
     marginBottom: 15,
     fontSize: 40,
     color: 'black',
@@ -225,7 +225,8 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontSize: 20,
-    marginBottom: 10,
+    marginBottom: 5,
+    marginTop: 10,
   },
   locationContainer: {
     flexDirection: 'row',
@@ -249,13 +250,19 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   locationTitle: {
-    fontSize: 20,
+    fontSize: 27,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 5,
+    color: 'black',
+    paddingHorizontal: 10,
+    borderWidth: 3,
+    borderColor: '#ffa970',
+    fontWeight: 'bold',
+    borderRadius: 10,
   },
   separator: {
     height: 3,
-    backgroundColor: 'gray',
+    backgroundColor: 'black',
     width: '100%',
     marginBottom: 10,
     marginTop: 20,
